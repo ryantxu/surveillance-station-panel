5c5b1780f82a78313b21833e4e10e460e063e78a
Ryan McKinley <ryantxu@gmail.com>
2017-11-01 18:41:30 +0100

    adding sid

--- src/module.ts
+++ src/module.ts
@@ -8,6 +8,9 @@ import {PanelCtrl} from  'app/plugins/sdk';
 import _ from 'lodash';
 import moment from 'moment';
 
+import './css/sss.css!';
+
+
 
 class SurveillanceStationCtrl extends PanelCtrl {
   static templateUrl = 'partials/module.html';
@@ -15,8 +18,16 @@ class SurveillanceStationCtrl extends PanelCtrl {
   defaults = {
     url: 'https://your-synology-server:5001/webapi/',
     account: 'user',
-    passwd: null
+    passwd: null,
+    show: [], // ids that we will see
   };
+
+  auth = {
+    count: 0,
+    sid: 'EdAYe8TV1ww6U1570NBN581800' // replace from query!!!
+  };
+
+  cameras: Array<any>;
 
   /** @ngInject **/
   constructor($scope, $injector, private templateSrv, private $http, private uiSegmentSrv, private datasourceSrv) {
@@ -38,13 +49,21 @@ class SurveillanceStationCtrl extends PanelCtrl {
 
   onRefresh() {
     console.log('On Refresh...');
+
+    let ts = Date.now();
+    _.forEach( this.cameras, camera => {
+      camera.snapshot_url = 'https://flow.natelenergy.com:5001'
+        + camera.snapshot_path
+        + '&_sid=' + this.auth.sid 
+        + '&_ts=' + ts
+    });
   }
 
   //-----------------
 
   connectionChanged() {
     delete this.error;
-    console.log('Connection changed');
+    console.log('Connection changed XX');
     if(!this.panel.url.endsWith('/')) {
       this.panel.url += '/';
     }
@@ -54,13 +73,42 @@ class SurveillanceStationCtrl extends PanelCtrl {
       return;
     }
 
-    this.api_AuthLogin();
+    this.api_ListCameras();
+  //  this.api_GetInfo();
+    //this.api_AuthLogin();
   }
 
+   
+  showFullscreen(camera) {
+    console.log( 'TODO, show live popup', camera ); 
+  }
 
   //-----------------
 
+  moveCamera( camera, direction ) {
+    var index = _.indexOf(this.cameras, camera);
+    _.move(this.cameras, index, index + direction);
+    this.configChanged();
+  }
+
+  configChanged() {
+    let show = [];
+    _.forEach( this.cameras, camera => {
+      if( camera._show ) {
+        show.push(camera.id);
+      }
+    });
+    this.panel.show = show;
+  }
+
+  //-----------------
+
+
+
   api_AuthLogin() {
+    console.log( "Do auth", this.panel );
+    this.auth.count++;
+    
     // auth.cgi?
     let params = {
       api: 'SYNO.API.Auth',
@@ -73,13 +121,85 @@ class SurveillanceStationCtrl extends PanelCtrl {
     };
 
     return this.$http({
-      url: this.panel.url + '/auth.cgi',
+      url: this.panel.url + 'auth.cgi',
       method: 'GET',
       params: params
     }).then((rsp) => {
       console.log( "Auth OK", rsp );
     }, err => {
       console.log( "Auth Error", err );
+      this.error = err; //.data.error + " ["+err.status+"]";
+      this.inspector = {error: err};
+    });
+  }
+
+
+  api_GetInfo() {
+    console.log( "Do GetInfo", this.panel );
+    
+    // auth.cgi?
+    let params = {
+      api: 'SYNO.SurveillanceStation.Info',
+      method: 'GetInfo',
+      version: 6,
+      _sid: this.auth.sid
+    };
+
+    return this.$http({
+      url: this.panel.url + 'entry.cgi',
+      method: 'GET',
+      params: params
+    }).then((rsp) => {
+      console.log( "GetInfo OK", rsp );
+    }, err => {
+      console.log( "GetInfo Error", err );
+      this.error = err; //.data.error + " ["+err.status+"]";
+      this.inspector = {error: err};
+    });
+  }
+
+  api_ListCameras() {
+    // auth.cgi?
+    let params = {
+      api: 'SYNO.SurveillanceStation.Camera',
+      method: 'List',
+      version: 8,
+      basic: true,
+      streamInfo: true,
+      _sid: this.auth.sid
+    };
+
+    return this.$http({
+      url: this.panel.url + 'entry.cgi',
+      method: 'GET',
+      params: params
+    }).then((rsp) => {
+      let cameras = rsp.data.data.cameras;
+      let count = 0;
+      for(let i=0; i<this.panel.show.length; i++) {
+        var index = _.findIndex(cameras, ['id', this.panel.show[i]]);
+        if(index >= 0) {
+          try {
+            cameras[index]._show = true;
+            _.move(cameras, index, i);
+            count++;
+          }
+          catch(err) {
+             console.log('error loading cameras', err, cameras);   
+          }
+        }
+      };
+      if(cameras<1) {
+        _.forEach(cameras, camera => {
+          camera._show = true;
+        });
+      }
+
+      this.cameras = cameras;
+      console.log( "api_ListCameras", this.cameras );
+      this.onRefresh();
+    }, err => {
+      console.log( "api_ListCameras", err );
       this.error = err; //.data.error + " ["+err.status+"]";
       this.inspector = {error: err};
     });
